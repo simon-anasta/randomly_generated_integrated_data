e237e1b344f6000311cf97e1eaabb6e26577e709
simon-anasta <simon.anastasiadis@gmail.com>
2022-12-11 17:44:29 +1300

    first raft DLR table

--- Code main/generate_drivers_licence_register_historic.R
+++ Code main/generate_drivers_licence_register_historic.R
@@ -101,6 +101,10 @@ C2_MAX_GAP = 365 * 5
 C345_MIN_GAP = 15
 C345_MAX_GAP = 365 * 8
 
+DLR_DATE = as.Date("2021-01-12")
+NULL_DATE = as.Date("9000-01-01")
+ORIGIN_DATE = as.Date("1000-01-01")
+
 ## setup ----------------------------------------------------------------------
 
 library(dplyr)
@@ -193,8 +197,8 @@ generated_base = base_population %>%
   mutate(
     c2_licence = runif(table_size) < C2_PROB_FULL,
     c2_licence_lost = runif(table_size) < C2_PROB_FULL_LOST,
-    days_gap1 = randbetween(C2_MIN_GAP, C_MAX_GAP, table_size),
-    days_gap2 = randbetween(C2_MIN_GAP, C_MAX_GAP, table_size),
+    days_gap1 = randbetween(C2_MIN_GAP, C2_MAX_GAP, table_size),
+    days_gap2 = randbetween(C2_MIN_GAP, C2_MAX_GAP, table_size),
     full_days1 = randbetween(FULL_LOST_MIN_DAYS, FULL_LOST_MAX_DAYS, table_size),
     full_days2 = randbetween(FULL_LOST_MIN_DAYS, FULL_LOST_MAX_DAYS, table_size)
   ) %>%
@@ -256,12 +260,12 @@ sampler = lookup %>%
   filter(column == "nzta_hist_licence_type_text") %>%
   make_weighted_options_array()
 
-generated_historic = generated_base %>%
+generated_base = generated_base %>%
   mutate(licence_type = sample(sampler, table_size, replace = TRUE)) %>%
   mutate(licence_type = ifelse(!any_non & licence_type == "DIPLOMATIC", "PERMANENT", licence_type))
 
 # no future licences an no licence after death
-generated_historic = generated_historic %>%
+generated_historic = generated_base %>%
   mutate(
     c1_learner_start_date = if_else(year(c1_learner_start_date) > MAX_YEAR | year(c1_learner_start_date) < MIN_YEAR | year(c1_learner_start_date) > snz_deceased_year_nbr, as.Date(NA), c1_learner_start_date),
     c1_restricted_start_date = if_else(year(c1_restricted_start_date) > MAX_YEAR | year(c1_restricted_start_date) < MIN_YEAR | year(c1_restricted_start_date) > snz_deceased_year_nbr, as.Date(NA), c1_restricted_start_date),
@@ -484,4 +488,261 @@ generated_historic %>%
   write.csv(GENERATED_FILE1, row.names = FALSE)
 
 ## HISTORIC TABLE COMPLETE ----------------------------------------------------
+rm("c1","c2","c3","c4","c5","c6", "generated_historic")
 
+## current licence status -----------------------------------------------------
+
+generated_dlr = generated_base %>%
+  # licence stage at DLR_DATE
+  mutate(
+    c1_stage = case_when(
+      c1_learner & !c1_learner_lost & c1_restricted & !c1_restricted_lost & c1_full & c1_full_start_date < DLR_DATE ~ "full",
+      c1_learner & !c1_learner_lost & c1_restricted & c1_restricted_start_date < DLR_DATE ~ "restricted",
+      c1_learner & c1_learner_start_date < DLR_DATE ~ "learner",
+      TRUE ~ "none"
+    ),
+    c2_stage = case_when(
+      c1_stage == "full" & c2_licence & !c2_licence_lost & c2_start_date < DLR_DATE ~ "full",
+      TRUE ~ "none"
+    ),
+    c3_stage = case_when(
+      c1_stage == "full" & c2_licence & c3_licence & !c3_licence_lost & c3_start_date < DLR_DATE ~ "full",
+      TRUE ~ "none"
+    ),
+    c4_stage = case_when(
+      c1_stage == "full" & c2_licence & c3_licence & c4_licence & !c4_licence_lost & c4_start_date < DLR_DATE ~ "full",
+      TRUE ~ "none"
+    ),
+    c5_stage = case_when(
+      c1_stage == "full" & c2_licence & c3_licence & c4_licence & c5_licence & !c5_licence_lost & c5_start_date < DLR_DATE ~ "full",
+      TRUE ~ "none"
+    ),
+    c6_stage = case_when(
+      c6_learner & !c6_learner_lost & c6_restricted & !c6_restricted_lost & c6_full & c6_full_start_date < DLR_DATE ~ "full",
+      c6_learner & !c6_learner_lost & c6_restricted & c6_restricted_start_date < DLR_DATE ~ "restricted",
+      c6_learner & c6_learner_start_date < DLR_DATE ~ "learner",
+      TRUE ~ "none"
+    )
+  ) %>%
+  # licence status at DLR_DATE
+  mutate(
+    c1_status = case_when(
+      c1_stage == "full" & c1_full_lost & c1_end < DLR_DATE ~ "suspended",
+      c1_stage == "full" ~ "active",
+      c1_stage == "restricted" & c1_restricted_lost & c1_restricted_start_date < DLR_DATE ~ "suspended",
+      c1_stage == "restricted" ~ "active",
+      c1_stage == "learner" & c1_learner_lost & c1_learner_start_date < DLR_DATE ~ "suspended",
+      c1_stage == "learner" ~ "active",
+      c1_stage == "none" ~ "none"
+    ),
+    c2_status = case_when(
+      c2_stage == "full" & c2_licence_lost & c2_end < DLR_DATE ~ "suspended",
+      c2_stage == "full" ~ "active",
+      c2_stage == "none" ~ "none"
+    ),
+    c3_status = case_when(
+      c3_stage == "full" & c3_licence_lost & c3_end < DLR_DATE ~ "suspended",
+      c3_stage == "full" ~ "active",
+      c3_stage == "none" ~ "none"
+    ),
+    c4_status = case_when(
+      c4_stage == "full" & c4_licence_lost & c4_end < DLR_DATE ~ "suspended",
+      c4_stage == "full" ~ "active",
+      c4_stage == "none" ~ "none"
+    ),
+    c5_status = case_when(
+      c5_stage == "full" & c5_licence_lost & c5_end < DLR_DATE ~ "suspended",
+      c5_stage == "full" ~ "active",
+      c5_stage == "none" ~ "none"
+    ),
+    c6_status = case_when(
+      c6_stage == "full" & c6_full_lost & c6_end < DLR_DATE ~ "suspended",
+      c6_stage == "full" ~ "active",
+      c6_stage == "restricted" & c6_restricted_lost & c6_restricted_start_date < DLR_DATE ~ "suspended",
+      c6_stage == "restricted" ~ "active",
+      c6_stage == "learner" & c6_learner_lost & c6_learner_start_date < DLR_DATE ~ "suspended",
+      c6_stage == "learner" ~ "active",
+      c6_stage == "none" ~ "none"
+    )
+  ) %>%
+  # licence open date
+  mutate(
+    c1_open_date = case_when(
+      c1_stage == "full" ~ c1_full_start_date,
+      c1_stage == "restricted" ~ c1_restricted_start_date,
+      c1_stage == "learner" ~ c1_learner_start_date,
+      TRUE ~ NULL_DATE
+    ),
+    c2_open_date = case_when(
+      c2_stage == "full" ~ c2_start_date, TRUE ~ NULL_DATE
+    ),
+    c3_open_date = case_when(
+      c3_stage == "full" ~ c3_start_date, TRUE ~ NULL_DATE
+    ),
+    c4_open_date = case_when(
+      c4_stage == "full" ~ c4_start_date, TRUE ~ NULL_DATE
+    ),
+    c5_open_date = case_when(
+      c5_stage == "full" ~ c5_start_date, TRUE ~ NULL_DATE
+    ),
+    c6_open_date = case_when(
+      c6_stage == "full" ~ c6_full_start_date,
+      c6_stage == "restricted" ~ c6_restricted_start_date,
+      c6_stage == "learner" ~ c6_learner_start_date,
+      TRUE ~ NULL_DATE
+    )
+  ) %>%
+  # licence close date
+  mutate(
+    c1_close_date = case_when(
+      c1_status != "suspended" ~ NULL_DATE,
+      c1_stage == "full" ~ c1_end,
+      c1_stage == "restricted" ~ c1_full_start_date,
+      c1_stage == "learner" ~ c1_restricted_start_date,
+      TRUE ~ NULL_DATE
+    ),
+    c2_close_date = case_when(
+      c2_stage == "full" & c2_status == "suspended" ~ c2_end,
+      TRUE ~ NULL_DATE
+    ),
+    c3_close_date = case_when(
+      c3_stage == "full" & c3_status == "suspended" ~ c3_end,
+      TRUE ~ NULL_DATE
+    ),
+    c4_close_date = case_when(
+      c4_stage == "full" & c4_status == "suspended" ~ c4_end,
+      TRUE ~ NULL_DATE
+    ),
+    c5_close_date = case_when(
+      c5_stage == "full" & c5_status == "suspended" ~ c5_end,
+      TRUE ~ NULL_DATE
+    ),
+    c6_close_date = case_when(
+      c6_status != "suspended" ~ NULL_DATE,
+      c6_stage == "full" ~ c6_end,
+      c6_stage == "restricted" ~ c6_full_start_date,
+      c6_stage == "learner" ~ c6_restricted_start_date,
+      TRUE ~ NULL_DATE
+    )
+  )
+
+## completing required columns ------------------------------------------------
+
+gg = generated_dlr %>%
+  mutate(
+    nzta_dlr_licence_issue_date = case_when(
+      c1_learner & !c6_learner ~ c1_learner_start_date,
+      !c1_learner & c6_learner ~ c6_learner_start_date,
+      c1_learner & c6_learner ~ pmin(c1_learner_start_date, c6_learner_start_date),
+      TRUE ~ NULL_DATE
+    ),
+    nzta_dlr_licence_stage_text = case_when(
+      c1_stage == "learner" ~ "learner",
+      c6_stage == "learner" ~ "learner",
+      c1_stage == "restricted" ~ "restricted",
+      c6_stage == "restricted" ~ "restricted",
+      c1_stage == "full" ~ "full",
+      c6_stage == "full" ~ "full",
+      TRUE ~ "none"
+    ),
+    nzta_dlr_licence_status_text = case_when(
+      c1_status == "suspended" ~ "suspended",
+      c6_status == "suspended" ~ "suspended",
+      c1_status == "active" ~ "active",
+      c6_status == "active" ~ "active",
+      TRUE ~ "none"
+    ),
+    nzta_dlr_class_status_text = case_when(
+      nzta_dlr_licence_status_text == "suspended" ~ "suspended",
+      c2_status == "suspended" ~ "suspended",
+      c3_status == "suspended" ~ "suspended",
+      c4_status == "suspended" ~ "suspended",
+      c5_status == "suspended" ~ "suspended",
+      nzta_dlr_licence_status_text == "active" & (
+        c2_status == "active" | c3_status == "active" | c4_status == "active" | c5_status == "active"
+      ) ~ "active"
+    ),
+    nzta_dlr_lic_class_grant_date = pmin(c2_open_date, c3_open_date, c4_open_date, c5_open_date),
+    nzta_dlr_licence_class_text = paste0(
+      ifelse(c1_status == "active", "1", ""),
+      ifelse(c2_status == "active", "2", ""),
+      ifelse(c3_status == "active", "3", ""),
+      ifelse(c4_status == "active", "4", ""),
+      ifelse(c5_status == "active", "5", ""),
+      ifelse(c6_status == "active", "6", "")
+    ),
+    nzta_dlr_licence_start_date = pmax(
+      if_else(c1_status == "active", c1_open_date, ORIGIN_DATE),
+      if_else(c2_status == "active", c2_open_date, ORIGIN_DATE),
+      if_else(c3_status == "active", c3_open_date, ORIGIN_DATE),
+      if_else(c4_status == "active", c4_open_date, ORIGIN_DATE),
+      if_else(c5_status == "active", c5_open_date, ORIGIN_DATE),
+      if_else(c6_status == "active", c6_open_date, ORIGIN_DATE),
+      if_else(c1_status == "suspended", c1_close_date, ORIGIN_DATE),
+      if_else(c2_status == "suspended", c2_close_date, ORIGIN_DATE),
+      if_else(c3_status == "suspended", c3_close_date, ORIGIN_DATE),
+      if_else(c4_status == "suspended", c4_close_date, ORIGIN_DATE),
+      if_else(c5_status == "suspended", c5_close_date, ORIGIN_DATE),
+      if_else(c6_status == "suspended", c6_close_date, ORIGIN_DATE)
+    ),
+    year_latest_licence_issue = (year(DLR_DATE) - year(nzta_dlr_licence_issue_date)) %/% 5 * 5,
+    years_latest_class_issue = (year(DLR_DATE) - year(nzta_dlr_lic_class_grant_date)) %/% 5 * 5,
+    nzta_dlr_licence_from_date = nzta_dlr_licence_issue_date + 365 * year_latest_licence_issue,
+    nzta_dlr_class_from_date = nzta_dlr_lic_class_grant_date + 365 * years_latest_class_issue
+  )
+
+## remaining randomly generated columns ---------------------------------------
+
+table_size = nrow(gg)
+
+lookup = read.csv(LOOKUP_FILE, stringsAsFactors = FALSE) %>%
+  filter(schema == "nzta_clean", table == "drivers_licence_register")
+
+sampler = lookup %>%
+  filter(column == "nzta_dlr_organ_donor_ind") %>%
+  make_weighted_options_array()
+
+gg = gg %>%
+  mutate(nzta_dlr_organ_donor_ind = sample(sampler, table_size, replace = TRUE))
+
+sampler = lookup %>%
+  filter(column == "nzta_dlr_endorsement_type_text") %>%
+  make_weighted_options_array()
+
+gg = gg %>%
+  mutate(nzta_dlr_endorsement_type_text = sample(sampler, table_size, replace = TRUE))
+
+sampler = lookup %>%
+  filter(column == "nzta_dlr_region_code") %>%
+  make_weighted_options_array()
+
+gg = gg %>%
+  mutate(nzta_dlr_region_code = sample(sampler, table_size, replace = TRUE))
+
+## tidy up / enforce consistency ----------------------------------------------
+
+
+## output dlr -----------------------------------------------------------------
+
+gg %>%
+  select(
+    snz_uid,
+    snz_nzta_uid,
+    nzta_snz_sex_code = snz_sex_gender_code,
+    nzta_dlr_birth_month_nbr = snz_birth_month_nbr,
+    nzta_dlr_birth_year_nbr = snz_birth_year_nbr,
+    nzta_dlr_licence_issue_date,
+    nzta_dlr_organ_donor_ind,
+    nzta_dlr_licence_type_text = licence_type,
+    nzta_dlr_licence_status_text,
+    nzta_dlr_lic_class_grant_date,
+    nzta_dlr_licence_start_date,
+    nzta_dlr_licence_from_date,
+    nzta_dlr_class_status_text,
+    nzta_dlr_licence_class_text,
+    nzta_dlr_licence_stage_text,
+    nzta_dlr_class_from_date,
+    nzta_dlr_endorsement_type_text,
+    nzta_dlr_region_code
+  ) %>%
+  write.csv(GENERATED_FILE2, row.names = FALSE)
